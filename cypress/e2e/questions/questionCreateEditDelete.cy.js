84d89750b04e464f16b8eb5646ed53f6008490c1
ATHULYA ATHULYA <athulya.s@techversantinfo.com>
2025-12-19 15:22:57 +0530

    QuestionCreateEditDelete

--- cypress/e2e/questions/questionCreateEditDelete.cy.js
+++ cypress/e2e/questions/questionCreateEditDelete.cy.js
@@ -6,20 +6,9 @@ import {
  
 describe('Tutor Create Questions Flow', () => {
  
-  // before(() => {
-  //   cy.clearAppData();
- 
-  //   // Load login data from fixture and login
-  //   cy.fixture('loginData').then((data) => {
-  //  cy.loginAsTutorUI(data.tutor.email, data.tutor.password);
-
-  //   });
-  // });
- 
 
   it('should create, edit and delete a question', () => {
 
-   // cy.loginTutorUI(data.tutor.email, data.tutor.password);
     createQuestion();
     editCreatedQuestion();
     deleteCreatedQuestion();
